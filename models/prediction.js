f0a576122f3fa5adc06d3d2a941b46e84e3c3ed4
mbd-s <michaelbsilverberg@gmail.com>
2016-04-07 14:37:24 -0700

    added prediction schema

---
+++ models/prediction.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PredictionSchema = new Schema({
+  name: String,
+  checkDate: String,
+  notes: String,
+  sourceDescr: String,
+  sourceUrl: String,
+  submitterName: String,
+  submitterEmail: String,
+  truthValue: Number
+});
+
+var Prediction = mongoose.model('Prediction', PredictionSchema);
+
+module.exports = Prediction;
