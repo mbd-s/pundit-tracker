6e88047d5456cb987cb6a5a7ca6da6a95bf0f883
mbd-s <michaelbsilverberg@gmail.com>
2016-04-08 10:50:04 -0700

    got rid of second page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,24 +3,33 @@
 
 $(document).ready(function() {
   //make an ajax call to my server
-  // $('#openModal').on('click', function(){
-//   $.ajax({
-//     method: "GET",
-//     url: "api/sanity",
-//     success: sanitySuccess,
-//     error: sanityError
-//   });
-// });
-//
-// });
-//
-// function sanitySuccess(json){
-//   console.log("Success!");
-//   console.log(json);
-//   // function to append something to page
-// }
-//
-// function sanityError(error){
-//   console.log("Error!");
-//   console.log(error);
+  $.ajax({
+    method: "GET",
+    url: "/api/pundit",
+    success: punditSuccess,
+    error: punditError
+  });
+
+  //TODO write openModal function
+  // on clicking openModal button, open modal
+
+  //TODO write punditCreate (or predictionCreate) function
+  //on submitting form
+  //prevent default
+  //AJAX call
+  // save to db
+  //render entry
+
 });
+
+function punditSuccess(json){
+console.log("Success!");
+console.log(json);
+//append past predictions to #targetPast
+//append future predictions to #targetFuture
+}
+
+function punditError(json){
+  console.log("Error!");
+  console.log(error);
+}
--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.static(__dirname + '/public'));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 /**********
  * ROUTES *
  **********/
@@ -23,11 +22,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function homepage(req, res) {
   console.log(__dirname);
   res.sendFile(__dirname + '/views/index.html');
-});
-
-app.get('/pundit-view', function showPunditView(req, res) {
-  console.log(__dirname);
-  res.sendFile(__dirname + '/views/pundit-view.html');
 });
 
 app.get('/admin', function showAdminPage(req, res) {
@@ -45,26 +39,26 @@ app.get('/admin', function showAdminPage(req, res) {
 //show all pundits
  app.get('/api/pundit', controllers.pundits.index);
 
- // //show one pundit
- // app.get('/api/pundit/:punditId', controllers.pundits.index);
-
- //show all predictions (is this necessary, since all predictions will be shown in 'show one pundit'?)
- // app.get('/api/pundit/:punditId/predictions', controllers.pundits.index);
+ // create a pundit (.create?)
+ // app.post('/api/pundit', controllers.pundits.create);
 
  //create a prediction embedded in pundit
  app.post('/api/pundit/:punditId/predictions/', controllers.pundits.create);
 
- // create a pundit
- // app.post('/api/pundit', controllers.pundits.create);
-
  //delete a pundit (admin only)
  app.delete('/api/pundit/:punditId', controllers.pundits.remove);
+
+ //update a pundit (admin only)
+ // app.put('/api/pundit/:punditId', controllers.pundits.update);
 
  //delete a prediction (admin only)
  // app.delete('api/prediction/:predictionId', controllers.punditsPredictions.delete);
 
- //update a pundit (admin only)
- // app.put('/api/pundit/:punditId', controllers.pundits.update);
+ // //show one pundit (diff name than 'index'? 'show'?)
+ // app.get('/api/pundit/:punditId', controllers.pundits.index);
+
+ //show all predictions (is this necessary, since all predictions will be shown in 'show one pundit'?)
+ // app.get('/api/pundit/:punditId/predictions', controllers.pundits.index);
 
  //update a prediction (admin only)
  //app.put('/api/prediction/:predictionId', controllers.punditsPredictions.update);
