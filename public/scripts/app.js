99e6b8228d5559979fa6efa8c74d9430c990869b
mbd-s <michaelbsilverberg@gmail.com>
2016-04-09 14:14:02 -0700

    rendering imperfect data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,18 @@
 /* CLIENT-SIDE JS
 */
 
-var allPundits;
+var allPundits = [];
 var allPredictions;
 var pastTemplate;
-var futureTemplate;
+var currentTemplate;
 
 $(document).ready(function() {
   $('#date-picker').datepicker({});
   //TODO add function so dates in the past can't be chosen
-  var pastSource = $('#templatePast').html();
-  pastTemplate = Handlebars.compile(pastSource);
-
-  var futureSource = $('#templateFuture').html();
-  futureTemplate = Handlebars.compile(futureSource);
+  var sourcePast = $('#templatePast').html();
+  pastTemplate = Handlebars.compile(sourcePast);
+  var sourceCurrent = $('#templateCurrent').html();
+  currentTemplate = Handlebars.compile(sourceCurrent);
 
   //make an ajax call to my server
   $.ajax({
@@ -23,7 +22,12 @@ $(document).ready(function() {
     error: punditError
   });
 
-  //TODO write punditCreate (or predictionCreate) function
+  //TODO write predictionFormCapture function
+  $('#submitPrediction').on('submit', function(event){
+    event.preventDefault();
+    console.log('New prediction serialized', $(this).serializeArray());
+    
+  });
   //on submitting form
   //prevent default
   //AJAX call
@@ -32,14 +36,25 @@ $(document).ready(function() {
 
 });
 
+function render() {
+  $('#targetPast').empty();
+  $('#targetCurrent').empty();
+  var punditHtml = pastTemplate({ pundit: allPundits });
+
+  //append past predictions to #targetPast
+  $('#targetPast').append(punditHtml);
+  console.log(allPundits);
+    //append current predictions to #targetCurrent
+  $('#targetCurrent').append(punditHtml);
+}
+
 function punditSuccess(json){
-console.log("Success!");
-console.log(json);
-//append past predictions to #targetPast
-//append future predictions to #targetFuture
+  allPundits = json;
+  console.log("Success!");
+  console.log(json);
+  render();
 }
 
 function punditError(json){
-  console.log("Error!");
-  console.log(error);
+  console.log("Error!", error);
 }
