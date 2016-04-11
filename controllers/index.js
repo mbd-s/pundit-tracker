9615660a45a06483e3f2cde7171049a3cd32a3b8
mbd-s <michaelbsilverberg@gmail.com>
2016-04-11 10:29:58 -0700

    ready for heroku deploy attempt #1

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,9 @@
+var mongoose = require("mongoose");
+mongoose.connect( process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URL ||
+                  "mongodb://localhost/pundit-tracker" );
+
+
 module.exports.api = require('./apiController');
 module.exports.pundits = require('./punditsController');
 module.exports.punditsPredictions = require('./punditsPredictionsController');
